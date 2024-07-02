2f497aacda63f4650aab9df8a65d0ac844226204
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-07-02 11:59:23 +0530

    Initial Commit

---
+++ HMSAdmin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HMSAdmin
+//
+//  Created by Kajal Choudhary on 02/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HMSAdmin/HMSAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMSAdminApp.swift
+//  HMSAdmin
+//
+//  Created by Kajal Choudhary on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HMSAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
