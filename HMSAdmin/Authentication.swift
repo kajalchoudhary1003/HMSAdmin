b928c3d552e71cb036f34e3edeab129917318921
Ansh Kalra <staransh24@gmail.com>
2024-07-08 15:12:03 +0530

    Code refactoring and Hospital Cards

--- HMSAdmin/Authentication.swift
+++ HMSAdmin/Authentication.swift
@@ -117,7 +117,7 @@ struct Authentication: View {
             
             switch emailDomain {
             case "superadmin.com":
-                navigateToScreen(screen: SuperAdminView())
+                navigateToScreen(screen: HospitalView())
                 
             case "admin.com":
                 navigateToScreen(screen: AdminView())
@@ -150,19 +150,6 @@ struct Authentication: View {
     }
 }
 
-// Initialize Color from hex string
-extension Color {
-    init(hex: String) {
-        let scanner = Scanner(string: hex)
-        _ = scanner.scanString("#")
-        var rgb: UInt64 = 0
-        scanner.scanHexInt64(&rgb)
-        let r = Double((rgb >> 16) & 0xFF) / 255.0
-        let g = Double((rgb >> 8) & 0xFF) / 255.0
-        let b = Double(rgb & 0xFF) / 255.0
-        self.init(red: r, green: g, blue: b)
-    }
-}
 
 #Preview {
     Authentication()
--- HMSAdmin/demo.swift
+++
@@ -1,8 +0,0 @@
-//
-//  demo.swift
-//  HMSAdmin
-//
-//  Created by Kajal Choudhary on 03/07/24.
-//
-
-import Foundation
