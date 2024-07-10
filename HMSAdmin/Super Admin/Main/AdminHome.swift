2e1444b9c03d134b0af4406aa92dfc86603a2207
HARSHIT SHARMA <OMSHARMA2508@gmail.com>
2024-07-10 17:36:32 +0530

    added navigation to hospitalList file

--- HMSAdmin/Super Admin/Main/AdminHome.swift
+++ HMSAdmin/Super Admin/Main/AdminHome.swift
@@ -168,34 +168,38 @@ struct AdminHome : View {
                 
                 
                 
-                HStack {
-                    Image(systemName: "plus.square.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 24, height: 24)
-                        .foregroundColor(Color("appColor"))
-                        .padding(8)
-                        .cornerRadius(8)
-                    
-                    Text("Hospitals")
-                        .font(.title2)
-                    
-                    Spacer()
-                    
-                    Text("03")
-                        .foregroundColor(Color("appColor"))
-                        .font(.title)
-                        .fontWeight(.bold)
-                    
-                    Image(systemName: "chevron.right")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 15, height: 15)
-                        .foregroundColor(Color("appColor"))
-                        .padding(8)
-                        .cornerRadius(8)
+                // added navigation to hospitalView()
+                NavigationLink(destination: HospitalView()){
+                    HStack {
+                        Image(systemName: "plus.square.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(Color("appColor"))
+                            .padding(8)
+                            .cornerRadius(8)
+                        
+                        Text("Hospitals")
+                            .font(.title2)
+                        
+                        Spacer()
+                        
+                        Text("03")
+                            .foregroundColor(Color("appColor"))
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Image(systemName: "chevron.right")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 15, height: 15)
+                            .foregroundColor(Color("appColor"))
+                            .padding(8)
+                            .cornerRadius(8)
+                    }
+                    .padding()
                 }
-                .padding()
+                
                 
                 .background(Color.white)
                 .cornerRadius(10)
