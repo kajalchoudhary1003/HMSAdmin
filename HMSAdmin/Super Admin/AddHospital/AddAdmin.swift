862f4b768ac9a04e4497c719c5290c0c3c795ae3
Anmol-kriti <85841209+Anmol-kriti@users.noreply.github.com>
2024-07-10 16:24:18 +0530

    Added multiple admin functionality.

---
+++ HMSAdmin/Super Admin/AddHospital/AddAdmin.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct AddAdminView: View {
+    @Binding var newAdmin: Admin
+    var onSave: (Admin) -> Void
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Admin Details")) {
+                    TextField("First Name", text: $newAdmin.name)
+                    TextField("Address", text: $newAdmin.address)
+                    TextField("Email", text: $newAdmin.email)
+                    TextField("Phone", text: $newAdmin.phone)
+                }
+            }
+            .navigationBarTitle("Add Admin", displayMode: .inline)
+            .navigationBarItems(leading: Button("Cancel") {
+                presentationMode.wrappedValue.dismiss()
+            }, trailing: Button("Save") {
+                onSave(newAdmin)
+                presentationMode.wrappedValue.dismiss()
+            })
+        }
+    }
+}
