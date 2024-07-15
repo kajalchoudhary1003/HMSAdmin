bdd1a1c9dcf8a83e072dca61dca2202c80295ab3
Naksh1994 <99979363+Naksh1994@users.noreply.github.com>
2024-07-15 10:50:27 +0530

    navigation from adminView to staffView is applied again.

--- HMSAdmin/Admin/AdminView.swift
+++ HMSAdmin/Admin/AdminView.swift
@@ -56,7 +56,7 @@ struct AdminView: View {
     private var hospitalDataSection: some View {
         Section(header: Text("Hospital Data").font(.headline).foregroundColor(.gray)) {
             dataCard(icon: "stethoscope", title: "Doctors", count: "80", destination: AnyView(ShowDoctors()))
-            dataCard(icon: "syringe", title: "Staffs", count: "120", destination: AnyView(Text("Staff Screen")))
+            dataCard(icon: "syringe", title: "Staffs", count: "120", destination: AnyView(StaffsView()))
             dataCard(icon: "gift", title: "Offers", count: "05", destination: AnyView(Text("Offers Screen")))
         }
     }
